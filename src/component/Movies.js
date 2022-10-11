02be1d3cc595257153f62aef2e47b4974b6a749f
Kaustav-Bhattacharya <108495055+Kaustav-Bhattacharya@users.noreply.github.com>
2022-10-11 13:15:57 +0530

    Update Movies.js

--- src/component/Movies.js
+++ src/component/Movies.js
@@ -2,13 +2,14 @@ import React from "react";
 import MovieItems from "./MovieItem";
 import { List } from "./dataFile/MoviesList";
 
-export default function Movies() {
+export default function Movies({theme , cardTheme}) {
   return (
-    <div className="col md-4 raw my-5">
+    <div className="col md-4 raw my-5" style={{backgroundColor: theme}}>
       {List.map((movies) => {
         return (
           <div key={movies.id} className="three my-3">
             <MovieItems
+              cardTheme={cardTheme}
               imageUrl={movies.img.src}
               imageAlt={movies.img.alt}
               year={movies.year}
